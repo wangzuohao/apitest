bf3a0706bd4ca29d59eca84b26fe3af5aa69c18c
WangZuoHao <85275362+wangzuohao@users.noreply.github.com>
2022-02-09 16:54:55 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.handler = async (event) => {
+  const min = 1;
+  const max = 6;    
+  const randomNumber = Math.floor(
+    Math.random() * (max - min + 1)
+  ) + min;
+  const message = 'Your dice throw resulted in: ' +
+    randomNumber;
+  return message;
+};
